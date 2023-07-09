ee711f1fb7db8931fe1a6c15b87148a24af23e48
Kafumio <linzehong1_2020@qq.com>
2023-07-09 14:41:18 +0800

    add lab_1

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,48 +1,81 @@
 package mr
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+)
 import "log"
 import "net/rpc"
 import "hash/fnv"
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
+type SortedKey []KeyValue
+
+// Len 重写len,swap,less才能排序
+func (k SortedKey) Len() int           { return len(k) }
+func (k SortedKey) Swap(i, j int)      { k[i], k[j] = k[j], k[i] }
+func (k SortedKey) Less(i, j int) bool { return k[i].Key < k[j].Key }
+
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
+	keepFlag := true
+	for keepFlag {
+		task := GetTask()
+		switch task.TaskType {
+		case MapTask:
+			{
+				DoMapTask(mapf, &task)
+				// fmt.Println("*****************Map****************")
+				callDone(&task)
+			}
+		case WaitingTask:
+			{
+				time.Sleep(time.Second * 5)
+				// fmt.Println("All tasks are in progress, please wait...")
+			}
+		case ReduceTask:
+			{
+				DoReduceTask(reducef, &task)
+				// fmt.Println("*****************Reduce*************")
+				callDone(&task)
+			}
+		case ExitTask:
+			{
+				time.Sleep(time.Second)
+				fmt.Println("All tasks are done, will be exiting...")
+				keepFlag = false
+			}
+		}
+	}
 
+	time.Sleep(time.Second)
 	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
-
 }
 
-//
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -67,11 +100,108 @@ func CallExample() {
 	}
 }
 
-//
+// GetTask 获取任务（需要知道是Map任务，还是Reduce）
+func GetTask() Task {
+	args := TaskArgs{}
+	reply := Task{}
+	ok := call("Coordinator.PollTask", &args, &reply)
+	if ok {
+		fmt.Println("worker get ", reply.TaskType, " task :Id[", reply.TaskId, "]")
+	} else {
+		fmt.Println("call failed!")
+	}
+	return reply
+}
+
+func DoMapTask(mapf func(string, string) []KeyValue, response *Task) {
+	var intermediate []KeyValue
+	fileName := response.FileSlice[0]
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalf("cannot open %v", fileName)
+	}
+	// 通过io工具包获取content，作为mapf的参数
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", fileName)
+	}
+	file.Close()
+
+	// map返回一组KV结构体数组
+	intermediate = mapf(fileName, string(content))
+	// initialize and loop over []KeyValue
+	rn := response.ReducerNum
+	// 创建一个长度为nReduce的二维切片
+	HashedKey := make([][]KeyValue, rn)
+	for _, kv := range intermediate {
+		HashedKey[ihash(kv.Key)%rn] = append(HashedKey[ihash(kv.Key)%rn], kv)
+	}
+	for i := 0; i < rn; i++ {
+		oName := "mr-tmp-" + strconv.Itoa(response.TaskId) + "-" + strconv.Itoa(i)
+		oFile, _ := os.Create(oName)
+		enc := json.NewEncoder(oFile)
+		for _, kv := range HashedKey[i] {
+			err := enc.Encode(kv)
+			if err != nil {
+				return
+			}
+		}
+		oFile.Close()
+	}
+}
+
+func DoReduceTask(reducef func(string, []string) string, response *Task) {
+	reduceFileNum := response.TaskId
+	intermediate := shuffle(response.FileSlice)
+	dir, _ := os.Getwd()
+	tempFile, err := ioutil.TempFile(dir, "mr-tmp-*")
+	if err != nil {
+		log.Fatal("Failed to create temp file", err)
+	}
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		var values []string
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+		fmt.Fprintf(tempFile, "%v %v\n", intermediate[i].Key, output)
+		i = j
+	}
+	tempFile.Close()
+
+	// 在完全写入后进行重命名
+	fn := fmt.Sprintf("mr-out-%d", reduceFileNum)
+	os.Rename(tempFile.Name(), fn)
+}
+
+// shuffle 洗牌方法，得到一组排序好的kv数组
+func shuffle(files []string) []KeyValue {
+	var kva []KeyValue
+	for _, filepath := range files {
+		file, _ := os.Open(filepath)
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			kva = append(kva, kv)
+		}
+		file.Close()
+	}
+	sort.Sort(SortedKey(kva))
+	return kva
+}
+
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
@@ -88,4 +218,17 @@ func call(rpcname string, args interface{}, reply interface{}) bool {
 
 	fmt.Println(err)
 	return false
+}
+
+// callDone Call RPC to mark the task as completed
+func callDone(f *Task) Task {
+	args := f
+	reply := Task{}
+	ok := call("Coordinator.MarkFinished", &args, &reply)
+	if ok {
+		// fmt.Println("worker finish :taskId[", args.TaskId, "]")
+	} else {
+		fmt.Println("call failed!")
+	}
+	return reply
 }
